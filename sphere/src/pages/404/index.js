3b0b6e3accf3d1dfcd930e129336d49d94ea5de0
oopsy <whoops.yao@gmail.com>
2020-06-07 17:45:30 +0800

    💄 sphere Design 404

--- sphere/src/pages/404/index.js
+++ sphere/src/pages/404/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import styles from './.module.css';
+import { Link } from 'react-router-dom';
+
+const tips = [
+    '哦豁，这个页面不见了',
+    '肆零肆咯'
+];
+
+const getSample = arr => arr[Math.floor(Math.random() * arr.length)];
 
 export default _ => (
-    <div>Oops, 404!</div>
+    <div className={styles.block}>
+        <h1 className={styles.title}>404</h1>
+        <blockquote className={styles.tip}>
+            {getSample(tips)}
+        </blockquote>
+        <Link to='/' className={styles.back}>返回主页</Link>
+    </div>
 );
