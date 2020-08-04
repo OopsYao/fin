bcd4a394992a8a34f0088d085c6228a891cab69e
oopsy <whoops.yao@gmail.com>
2020-08-04 22:49:16 +0800

    🚧 strapi Use slate as editor

--- packages/strapi-cms/extensions/content-manager/admin/src/components/WysiwygWithErrors/index.js
+++ packages/strapi-cms/extensions/content-manager/admin/src/components/WysiwygWithErrors/index.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { Editor, EditorState, RichUtils } from 'draft-js';
-import 'draft-js/dist/Draft.css';
+import React from 'react';
+import Editor from '../Editor';
 
 const WysiwygWithErrors = ({
     inputDescription,
@@ -11,18 +10,7 @@ const WysiwygWithErrors = ({
     onChange, // : ({ target: { name, value } }) => {} 保存这个值至该field
     value,
 }) => {
-    const [editorState, setEditorState] = useState(
-        () => EditorState.createEmpty(),
-    );
-
-    const onBoldClick = e => {
-        e.preventDefault();
-        setEditorState(RichUtils.toggleInlineStyle(editorState, 'BOLD'));
-    };
-    return (<div>
-        <button onClick={onBoldClick}>Bold</button>
-        <Editor editorState={editorState} onChange={setEditorState} />
-    </div>)
+    return <Editor />
 };
 
 export default WysiwygWithErrors;
